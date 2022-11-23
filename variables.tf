1a45d577d28d79eac62117e4fdd502835ed2f8bb
Oleksii Nazarov <68003098+Hurr1son@users.noreply.github.com>
2022-11-23 11:37:55 +0200

    vars file

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "project" {
+  default   = "deploy-315515"
+}
+variable "zone" {
+  default   = "us-east1-c"
+}
+variable "region" {
+  default   = "us-east1"
+}
+variable "fw-name" {
+  default   = "webg"
+}
+variable "fw_rages" {
+  default = ["0.0.0.0/0"]
+}
+variable "inst_name"{
+ default ="vm-address" 
+}
